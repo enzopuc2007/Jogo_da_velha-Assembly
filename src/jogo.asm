a0284bc5d0472ae391ace1dcf57111b65f959154
gabriel-baptista-0607 <gabriel.baptista@ogfigovernance.com>
2025-11-23 11:31:02 -0300

    feat: added commented content (partially commented)

--- src/jogo.asm
+++ src/jogo.asm
@@ -1,480 +1,57 @@
-TITLE JOGO
-.MODEL SMALL
-.STACK 100h
-.DATA
-  MSG_BEMVINDO DB 'JOGO DA VELHA $'
-  MSG1 DB 'Selecione o modo de jogo (0 - Multiplayer | 1 - Computador)', 13, 10, 13, 10,'Digite a sua opcao: $'
-  MSG2 DB 10,10,'Tente novamente, digito nao reconhecido...',13, 13, 10, '$'
-  MATRIZ DB 31H, 32H, 33H
+TITLE JOGO_DA_VELHA_OSC
+; Este trabalho foi realizado durante as aulas prática de Organização de Sistemas de Computação
+; pelos alunos de Engenharia de Computação da Pontifícia Universidade Católica de Campinas 
+; Enzo Marchi Romera e Gabriel de Oliveira Baptista
+.MODEL SMALL ; o modelo de código deste programa Assembly x8086 é SMALL
+.STACK 100h ; definição do tamanho da pilha
+.DATA ; início do segmento de dados;
+  MSG_BEMVINDO DB 'JOGO DA VELHA $' ; mensagem de boas-vindas direcionada ao usuário
+  MSG1 DB 'Selecione o modo de jogo (0 - Multiplayer | 1 - Computador)', 13, 10, 13, 10,'Digite a sua opcao: $' ; mensagem de seleção de modo de jogo
+  MSG2 DB 10,10,'Tente novamente, digito nao reconhecido...',13, 13, 10, '$' ; caso o dígito para a entrada do ti de jogo não for conhecida, esta mensagem será exibida
+  MATRIZ DB 31H, 32H, 33H ; declaração inicial da matriz com a numeração das posições dos dígitos.
          DB 34H, 35H, 36H
          DB 37H, 38H, 39H
 
-  MSG_ZERO DB 13, 10, 'A opcao selecionada foi a opcao MULTIPLAYER.', 13, 10, '$'
-  MSG_UM DB 13, 10, 'A opcao selecionada foi a opcao JOGO COM COMPUTADOR.', 13, 10, '$'
+  MSG_ZERO DB 13, 10, 'A opcao selecionada foi a opcao MULTIPLAYER.', 13, 10, '$' ; Esta mensagem será exibida caso o jogador selecione a opção de multijogador
+  MSG_UM DB 13, 10, 'A opcao selecionada foi a opcao JOGO COM COMPUTADOR.', 13, 10, '$' ; Esta mensagem será exibida caso o jogador selecione a opção de jogo contra a máquina 
 
-  MSG_CAPTA_JOG DB 'Escolha o numero da jogada(Opcoes: 1 a 9): $'
+  MSG_CAPTA_JOG DB 'Escolha o numero da jogada(Opcoes: 1 a 9): $' ; Esta mensagem será exibida para o jogador selecionar a posição de jogada.
 
-  MSG_INVALIDO DB 10,'Posicao invalida.',10,13,'Tente novamente...',10,13,10,'$'
+  MSG_INVALIDO DB 10,'Posicao invalida.',10,13,'Tente novamente...',10,13,10,'$' ; Esta mensgame será exibida caso o jogador insira uma posição fora do intervalo de possibildades de entrada (1-9) ou em uma posição já ocupada
 
-  MSG_EMPATE DB 'Empate! $'
+  MSG_EMPATE DB 'Empate! $' ; Esta mensagem será exibida em caso de jogo terminar em empate ou em "velha"
 
-  MSG_VEZ_J1 DB 'Vez do jogador 1 $'
-  MSG_VEZ_J2 DB 'Vez do jogador 2 $'
-  MSG_VEZ_ROBO DB 'Vez do robo $'
+  MSG_VEZ_J1 DB 'Vez do jogador 1 $' ; Mensagem exibida quando for a vez do jogador 1 jogar
+  MSG_VEZ_J2 DB 'Vez do jogador 2 $' ; Mensagem exibida quando for a vez do jogador 2 jogar
+  MSG_VEZ_ROBO DB 'Vez do robo $' ; Mensagem exibida quando for a vez do robô jogar
 
-  MSG_VIT_JOG DB 'O jogador 1 eh o vencedor $'
-  MSG_VIT_JOG2 DB 'O jogador 2 eh o vencedor $'
-  MSG_VIT_COMP DB 'O robo eh o vencedor $'
+  MSG_VIT_JOG DB 'O jogador 1 eh o vencedor $' ; Mensagem exibida quando o jogador 1 for o vencedor
+  MSG_VIT_JOG2 DB 'O jogador 2 eh o vencedor $' ; Mensagem exibida quando o jogador 2 for o vencedor
+  MSG_VIT_COMP DB 'O robo eh o vencedor $' ; Mensagem exibida quando o robô no modo de jogo 1 for vencedor
 
-  MSG_FINAL DB 'Deseja jogar novamente?(1-Sim / 2-Nao): $'
+  MSG_FINAL DB 'Deseja jogar novamente?(1-Sim / 2-Nao): $' ; Mensagem exibida ao fim de cada rodada para saber se o jogador gostaria de jogar outra partida no mesmo modo.
 
-  MSG_QUALQUER_TECLA DB 'Pressione qualquer tecla para continuar...$'
-.CODE 
-  INCLUDE macros.inc
- JOGO_MULTIPLAYER PROC
-  MOV CX,9
-JOGO1:
-  PUSH CX
-  CLS
-  TEXTO_INICIO
-  PULA_LINHA
-  PULA_LINHA
-  CALL IMPRIME_MATRIZ
-  PULA_LINHA
-  CALL CAPTA_VERIFICA
-  PULA_LINHA
-  CMP AX,1
-  JE SAI_DEFINITIVO
-  CMP AX,2
-  JE SAI_DEFINITIVO
-  POP CX
-  LOOP JOGO1
-  CALL IMPRIME_MATRIZ
-  RET
-SAI_DEFINITIVO:
-  POP CX
-  CALL IMPRIME_MATRIZ
-  RET
- JOGO_MULTIPLAYER ENDP
-
- JOGO_ROBO PROC
-  RET
- JOGO_ROBO ENDP
-
- CAPTA_VERIFICA PROC
-VOLTA:
-  MOV AH,9
-  LEA DX,MSG_CAPTA_JOG
-  INT 21h
-
-  MOV AH,1
-  INT 21h
-    
-  XOR BX,BX
-  XOR SI,SI
-
-  AND AL,0Fh
-
-  TEST AL,15
-  JZ ERRO
-
-  CMP AL,3
-  JBE ROT1
-  CMP AL,6
-  JBE ROT2
-  CMP AL,9
-  JBE ROT3
-
-ERRO:
-  TENTE_NOVAMENTE
-  JMP VOLTA
-
-ROT1:
-  MOV BL,AL
-  DEC BL
-  MOV SI,BX
-  MOV BL,0
-  JMP TEST1
-ROT2:
-  MOV BL,AL
-  SUB BL,4
-  MOV SI,BX
-  MOV BL,3
-  JMP TEST1
-ROT3:
-  MOV BL,AL
-  SUB BL,7
-  MOV SI,BX
-  MOV BL,6
-
-TEST1:
-  CALL VERIFICA_MATRIZ
-
-  TEST AX,1
-  JZ CONT_MAT
-
-  TENTE_NOVAMENTE
-  JMP VOLTA
-
-CONT_MAT:
-  CALL INSERE_MATRIZ
-  CALL VER_VIT
-  CMP AX,0
-  JE FINALIZA
-  CMP AX,1
-  JE VIT_1
-  CMP AX,2
-  JE VIT_2
-
-VIT_1:
-  CLS
-  MSG_VIT_1
-  JMP FINALIZA
-VIT_2:
-  CLS
-  MSG_VIT_2
-
-FINALIZA:
-  RET
- CAPTA_VERIFICA ENDP
-
- INSERE_MATRIZ PROC
-  TEST CX,1
-  JNZ JOG1
-  MOV MATRIZ[BX][SI],6Fh
-  JMP FINAL
-
-JOG1:
-  MOV MATRIZ[BX][SI],78h
-
-FINAL: 
-  RET
- INSERE_MATRIZ ENDP
-
-
- VERIFICA_MATRIZ PROC
-  XOR AX,AX
-
-  CMP MATRIZ[BX][SI],78h
-  JZ SAI8
-  CMP MATRIZ[BX][SI],6Fh
-  JZ SAI8
-  CMP MATRIZ[BX][SI],31h
-  JAE CONTINUA
-  CMP MATRIZ[BX][SI],39h
-  JBE CONTINUA
-
-SAI8:
-  MOV AX,1
-
-CONTINUA:
-  RET
- VERIFICA_MATRIZ ENDP
-
- TENTE_NOVAMENTE PROC
-  PUSH AX
-  PUSH DX
-
-  MOV AH,9
-  LEA DX,MSG_INVALIDO
-  INT 21h
-
-  CALL IMPRIME_MATRIZ
-  
-  POP DX
-  POP AX
-  RET
- TENTE_NOVAMENTE ENDP
-
- IMPRIME_MATRIZ PROC
-  PUSH AX
-  PUSH BX
-  PUSH CX
-  PUSH DX
-  XOR BX,BX
-  MOV AH,2
-  MOV CH,3
-
-REPETE2:
-  MOV CL,3
-  XOR SI,SI
-  MOV DL,20h
-  INT 21h
-REPETE1:
-  MOV AL,MATRIZ[BX][SI]
-
-  CMP AL,6Fh
-  JE IMPRIME0
-
-  CMP AL,78h
-  JE IMPRIMEX
-
-  JMP NUMERO
-
-IMPRIME0:
-  MOV DL,6Fh
-  INT 21h
-  JMP COND
-
-IMPRIMEX:
-  MOV DL,78h
-  INT 21h
-  JMP COND
-
-NUMERO:
-  MOV DL,AL
-  INT 21H
-  JMP COND
-
-COND:
-  CMP CL,1
-  JE FINAL1
-  MOV DL,7Ch
-  INT 21h  
-
-FINAL1:
-  INC SI
-  DEC CL
-  JNZ REPETE1
-  ADD BX,3
-
-  CMP CH,1
-  JE FINAL2
-
-  MOV DL,10
-  INT 21h
-
-  MOV DL,20h
-  INT 21h
-
-  MOV DH,3
-DENOVO:
-  MOV DL,2Dh
-  INT 21h
-
-  CMP DH,1
-  JE FINAL2
-
-  MOV DL,7Ch
-  INT 21h
-  DEC DH
-  JNZ DENOVO
-
-FINAL2:
-  MOV DL,10
-  INT 21h
-  DEC CH
-  JNZ REPETE2
-
-  POP DX
-  POP CX
-  POP BX
-  POP AX
-  RET
- IMPRIME_MATRIZ ENDP
-
- IMPRIME_UM PROC
-  MOV AH, 09H
-  MOV DX, OFFSET MSG_UM
-  INT 21H
-
-  RET
- IMPRIME_UM ENDP
-
- INICIALIZACAO PROC
- ; PUSHALL
-
-  ; IMPRIME MENSAGEM DE BEM-VINDO
-  MOV AH, 09H
-  MOV DX, OFFSET MSG_BEMVINDO
-  INT 21H
-
-  PULA_LINHA
-  PULA_LINHA
-
-ENTRADA_OPCAO:
-  ; IMPRIME MENSAGEM DE MODO DE JOGO
-  MOV DX, OFFSET MSG1
-  INT 21H
-
-  MOV AH, 01H
-  INT 21H
-
-  AND AL, 0FH
-
-  CMP AL, 0
-  JZ VOLTAR
-  CMP AL, 1
-  JE VOLTAR
-
-  MOV AH, 09H
-  MOV DX, OFFSET MSG2
-  INT 21H
-  JMP ENTRADA_OPCAO
-
-VOLTAR:
-      ; POPALL
-  RET 
- INICIALIZACAO ENDP
-
-VER_VIT PROC
- ;Verificação horizontal
-  XOR BX,BX
-  MOV CL,3
-
-DENOVO1:
-  XOR DX,DX
-  XOR SI,SI
-  MOV CH,3
-
-AGAIN1:
-  CMP MATRIZ[BX][SI],78h
-  JNE CONT1
-  INC DH
-CONT1:
-  CMP MATRIZ[BX][SI],6Fh
-  JNE SAI1
-  INC DL
-SAI1:
-  INC SI
-  DEC CH
-  JNZ AGAIN1
-
-  CMP DH,3
-  JE CORRECTION1
-  CMP DL,3
-  JE CORRECTION2
-  ADD BX,3
-  DEC CL
-  JNZ DENOVO1
-
-  ;Verificação vertical
-  XOR SI,SI
-  MOV CL,3
-DENOVO2:
-  XOR DX,DX
-  XOR BX,BX
-  MOV CH,3
-AGAIN2:
-  CMP MATRIZ[BX][SI],78h
-  JNE CONT2
-  INC DH
-CONT2:
-  CMP MATRIZ[BX][SI],6Fh
-  JNE SAI12
-  INC DL
-SAI12:
-  ADD BX,3
-  DEC CH
-  JNZ AGAIN2
-
-  CMP DH,3
-CORRECTION1:
-  JE VIT_JOG
-  CMP DL,3
-CORRECTION2:
-  JE VIT_JOG2_COMP
-  INC SI
-  DEC CL
-  JNZ DENOVO2
-      
-  XOR BX,BX
-  XOR SI,SI
-  XOR DX,DX
-  MOV CX,3
-AGAIN3:
-  CMP MATRIZ[BX][SI],78h
-  JNE CONT3
-  INC DH
-CONT3:
-  CMP MATRIZ[BX][SI],6Fh
-  JNE SAI3
-  INC DL
-SAI3:
-  ADD BX,3
-  INC SI
-  LOOP AGAIN3
-  CMP DH,3
-  JE VIT_JOG
-  CMP DL,3
-  JE VIT_JOG2_COMP
- 
-  XOR DX,DX
-  XOR BX,BX
-  MOV SI,2
-  MOV CX,3
-AGAIN4:
-  CMP MATRIZ[BX][SI],78h
-  JNE CONT4
-  INC DH
-CONT4:
-  CMP MATRIZ[BX][SI],6Fh
-  JNE SAI4
-  INC DL
-SAI4:
-  ADD BX,3
-  DEC SI
-  LOOP AGAIN4
-  CMP DH,3
-  JE VIT_JOG
-  CMP DL,3
-  JE VIT_JOG2_COMP
-  JMP RETORNA
-VIT_JOG:
-  CLS
-  MOV AX,1
-  JMP FINAL_VIT
-VIT_JOG2_COMP:
-  CLS
-  MOV AX,2
-  JMP FINAL_VIT
-RETORNA:
-  MOV AX,0
-FINAL_VIT:
-  RET
- VER_VIT ENDP
-
- RESET_MATRIZ PROC
-  PUSH BX
-  PUSH SI
-
-  XOR CX,CX
-  XOR BX,BX
-  MOV DL,31h
-  MOV CH,3
-
-RESET_MAT2:
-  XOR SI,SI
-  MOV CL,3
-RESET_MAT1:
-  MOV MATRIZ[BX][SI],DL
-  INC DL
-  INC SI
-  DEC CL
-  JNZ RESET_MAT1
-  ADD BX,3
-  DEC CH
-  JNZ RESET_MAT2
-
-  POP SI
-  POP BX
-  RET
- RESET_MATRIZ ENDP
-  
-
- MAIN PROC 
+  MSG_QUALQUER_TECLA DB 'Pressione qualquer tecla para continuar...$' ; mensagem exibida após a tratativa de um erro para que o fluxo do programa continue 
+.CODE ; início do segmento de código
+  ; Durante o desenvolvimento deste jogo, trabalhamos com a modularização, ou seja, separamos vários procedimentos e macros em diferentes arquivos a fim de deixar o programa principal (este arquivo aqui) mais enxuto e fácil de compreender no caso de outros leitores terem acesso a ele e quiserem editá-lo ou fazer manutenção no código.
+  INCLUDE macros.inc ; importa o arquivo de definição e de declaração com as MACROS
+  INCLUDE procs.inc ; importa o arquivo de definição e de declaração com os procedimentos
+ MAIN PROC ; declaração do programa principal
   MOV AX,@DATA      ;Inicialização dos dados
   MOV DS,AX
-  XOR DI,DI
+  XOR DI,DI ; limpa o registrador DI
 
-  CLS
+  CLS ; Executa a macro de limpeza de tela.
 
   CALL INICIALIZACAO    ;Iniciação do jogo
 
-  CLS
+  CLS ; Executa a macro de limpeza de tela.
 
   CMP AL, 0         ;Condição para entrar no modo multiplayer
-  JZ ESCOPO_ZERO    ;Condição 
+  JZ ESCOPO_ZERO
+  ; Se AL (entrada do usuário) for zero, entre no escopo de acesso ao jogo multiplayer
+  ; Se AL (entrada do usuário) for um, entre no escopo de acesso ao jogo contra o computador
+  ; Se AL (entrada do usuário) for um dígito não reconhecido, imprima uma mensagem de erro e repita a leitura inicial
 
 ESCOPO_UM:
 JOG_NOV1:
@@ -485,12 +62,12 @@ JOG_NOV1:
 
 ESCOPO_ZERO:
 JOG_NOV2:  
-  CALL JOGO_MULTIPLAYER
-  PERGUNTA_FINAL2
-  JMP FIM_PROGRAMA
+  CALL JOGO_MULTIPLAYER ; executa o procedimento de jogo multiplayer
+  PERGUNTA_FINAL2 ; executa a macro de pergunta d
+  JMP FIM_PROGRAMA ; 
 
 FIM_PROGRAMA:
   MOV AH, 4CH     ;Devolve o controle 
-  INT 21H
+  INT 21H         ; Fim do programa
  MAIN ENDP
 END MAIN
